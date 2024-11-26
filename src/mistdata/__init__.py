6a916202a38b46a01ef0f5160cfa77ed765a48de
Christian Hellum Bye <chbye@berkeley.edu>
2024-11-25 17:09:29 -0800

    import NoiseWave class

--- src/mistdata/__init__.py
+++ src/mistdata/__init__.py
@@ -4,4 +4,5 @@ from .DUTLNA import DUTLNA
 from .Spectrum import Spectrum
 from .MISTData import MISTData
 from .calibration import MISTCalibration
+from .noisewave import NoiseWave
 from .LSTBinnedSpectra import LSTBinnedSpectra, LSTBinnedTherm
