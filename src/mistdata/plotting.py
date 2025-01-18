e32b2bc646b9e8c853f7ae3f4fd6e4b72cf74dd0
Christian Hellum Bye <chbye@berkeley.edu>
2025-01-18 11:51:08 -0800

    add option to plot local time in datetime fmt

--- src/mistdata/plotting.py
+++ src/mistdata/plotting.py
@@ -1,37 +1,58 @@
 from typing import Sequence
 
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from datetime import datetime
 
 
-def plot_spec(x: Sequence, y: Sequence, spec: np.ndarray, vmin=1e11, vmax=3.3e11, cmap='plasma', plot_index=False):
+def plot_spec(
+    x: Sequence,
+    y: Sequence,
+    spec: np.ndarray,
+    vmin=1e11,
+    vmax=3.3e11,
+    cmap="plasma",
+    plot_index=False,
+    plot_datetime=False,
+):
     """
-    :param x: A sequence representing the x-axis values (frequency values in MHz).
+    :param x: A sequence representing the x-axis values (frequency in MHz).
     :param y: A sequence representing the y-axis values (time values).
     :param spec: A numpy ndarray representing the spectra.
-    :param lim: A tuple specifying the minimum and maximum values for the colorbar limits.
-                Default value is (1e11, 3.3e11).
+    :param lim: A tuple specifying the minimum and maximum values for the
+        colorbar limits. Default value is (1e11, 3.3e11).
     :param vmin: Minimum value for the lower colorbar limit.
     :param vmax: Minimum value for the upper colorbar limit.
-    :param cmap: A string specifying the color map for the spectrogram. Default value is 'plasma'.
+    :param cmap: A string specifying the color map for the spectrogram.
+        Default value is 'plasma'.
+    :param plot_index: A boolean flag indicating whether to plot the index on
+        the y-axis.
+    :param plot_datetime: A boolean flag indicating whether to plot the
+        datetime on the y-axis.
 
     :return: A matplotlib Figure object representing the plotted spectrogram.
     """
     if plot_index:
         ylabel = "Index"
         yextent = (len(spec), 0)
-    else:
-        if isinstance(y[0], datetime):
-            timespansec: int = (y[-1] - y[0]).total_seconds()
-            time_up_lim = timespansec / 60 if timespansec <= 10800 else timespansec / 3600
-            yextent = (time_up_lim, 0)
-            ylabel = "Time [min]" if timespansec <= 10800 else "Time [h]"
-        elif isinstance(y[0], float):
-            yextent = (y[-1], y[0])
-            ylabel = "LST [h]"
+    elif plot_datetime:
+        yextent = (y[-1], y[0])
+        ylabel = "Local Time"
+    elif isinstance(y[0], datetime):
+        timespansec: int = (y[-1] - y[0]).total_seconds()
+        if timespansec > 10800:
+            time_up_lim = timespansec / 3600
+            ylabel = "Time [h]"
         else:
-            raise ValueError("Unrecognized x-axis type")
+            time_up_lim = timespansec / 60
+            ylabel = "Time [min]"
+        yextent = (time_up_lim, 0)
+    elif isinstance(y[0], float):
+        yextent = (y[-1], y[0])
+        ylabel = "LST [h]"
+    else:
+        raise ValueError("Unrecognized y-axis type")
 
     fig = plt.figure(figsize=(8.25, 6))
     plt.imshow(
@@ -41,12 +62,15 @@ def plot_spec(x: Sequence, y: Sequence, spec: np.ndarray, vmin=1e11, vmax=3.3e11
         aspect="auto",
         interpolation="none",
         cmap=cmap,
-        extent=[np.min(x), np.max(x), *yextent]
+        extent=[np.min(x), np.max(x), *yextent],
     )
     if plot_index:
         ax = plt.gca()
         ax.set_yticks(np.arange(0, len(spec), 5))
-        plt.grid(alpha=1, ls='--', axis='y', which='major')
+        plt.grid(alpha=1, ls="--", axis="y", which="major")
+    elif plot_datetime:
+        ax = plt.gca()
+        ax.yaxis.set_major_formatter(mdates.DateFormatter("%D %H:%M"))
     cb = plt.colorbar()
     cb.ax.tick_params(size=5)
     plt.xlabel(r"Frequency, MHz")
@@ -60,23 +84,26 @@ def plot_spec_rows(x, spec, rows, vmin=1e11, vmax=4e11):
     :param x: Array of x-coordinates for the plot.
     :param spec: 2D array of y-coordinates for each row.
     :param rows: Sequence of row indices to plot.
-    :param limits: Tuple of lower and upper limits for the y-axis. Default is (1e11, 4e11).
+    :param limits: Tuple of lower and upper limits for the y-axis.
+        Default is (1e11, 4e11).
     :return: The plot figure.
 
-    This method plots the specified rows of a given spectrum data. The x-coordinates
-    for the plot are provided by the `x` parameter, while the y-coordinates are determined
-    by the `spec` parameter. The `spec` parameter should be a 2D array where each row
-    represents a different spectrum and each column represents a different x-coordinate.
+    This method plots the specified rows of a given spectrum data. The
+    x-coordinates for the plot are provided by the `x` parameter, while the
+    y-coordinates are determined by the `spec` parameter. The `spec` parameter
+    should be a 2D array where each row represents a different spectrum and
+    each column represents a different x-coordinate.
 
-    The `rows` parameter should be a sequence of row indices that you want to plot. By
-    specifying specific row indices, you can choose which spectra to plot. Each row will
-    be plotted as a separate line on the plot.
+    The `rows` parameter should be a sequence of row indices that you want to
+    plot. By specifying specific row indices, you can choose which spectra to
+    plot. Each row will be plotted as a separate line on the plot.
 
-    The `limits` parameter is an optional tuple that specifies the lower and upper limits
-    for the y-axis. By default, the limits are set to (1e11, 4e11).
+    The `limits` parameter is an optional tuple that specifies the lower and
+    upper limits for the y-axis. By default, the limits are set to
+    (1e11, 4e11).
 
-    This will plot the first, third, and fifth rows of the `spec` array against the `x` values,
-    with the y-axis limited to the range between 1e10 and 1e12.
+    This will plot the first, third, and fifth rows of the `spec` array against
+    the `x` values, with the y-axis limited to the range between 1e10 and 1e12.
     """
     fig = plt.figure(figsize=(8.25, 6))
     for row in rows:
